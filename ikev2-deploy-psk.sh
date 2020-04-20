743e479c96843293d759e452ca5b14a8a939da87
Quyen <quyen@Quyens-MacBook-Pro.local>
2020-04-20 22:00:54 +0700

    a

--- ikev2-deploy-psk.sh
+++ ikev2-deploy-psk.sh
@@ -12,7 +12,7 @@ if [ $(lsb_release -i -s) != "Ubuntu" ] || [ $(lsb_release -r -s) != "16.04" ]; 
 fi
 
 export SHARED_KEY=$(uuidgen)
-export IP=$(curl -s api.ipify.org)
+export IP=159.8.21.86
 
 echo "Your shared key (PSK) is $SHARED_KEY and your IP is $IP"
 echo -e "Press enter to continue...\n"; read
@@ -54,7 +54,7 @@ conn ikev2-vpn
     right=%any
     rightid=%any
     rightdns=8.8.8.8,8.8.4.4
-    rightsourceip=10.10.10.0/24
+    rightsourceip=159.8.21.80/29
     authby=secret
 EOF
 
@@ -92,11 +92,11 @@ iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -p udp --dport  500 -j ACCEPT
 iptables -A INPUT -p udp --dport 4500 -j ACCEPT
 
-iptables -A FORWARD --match policy --pol ipsec --dir in  --proto esp -s 10.10.10.10/24 -j ACCEPT
-iptables -A FORWARD --match policy --pol ipsec --dir out --proto esp -d 10.10.10.10/24 -j ACCEPT
-iptables -t nat -A POSTROUTING -s 10.10.10.10/24 -o eth0 -m policy --pol ipsec --dir out -j ACCEPT
-iptables -t nat -A POSTROUTING -s 10.10.10.10/24 -o eth0 -j MASQUERADE
-iptables -t mangle -A FORWARD --match policy --pol ipsec --dir in -s 10.10.10.10/24 -o eth0 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
+iptables -A FORWARD --match policy --pol ipsec --dir in  --proto esp -s 10.105.141.64/26 -j ACCEPT
+iptables -A FORWARD --match policy --pol ipsec --dir out --proto esp -d 10.105.141.64/26 -j ACCEPT
+iptables -t nat -A POSTROUTING -s 10.105.141.64/26 -o eth0 -m policy --pol ipsec --dir out -j ACCEPT
+iptables -t nat -A POSTROUTING -s 10.105.141.64/26 -o eth0 -j MASQUERADE
+iptables -t mangle -A FORWARD --match policy --pol ipsec --dir in -s 10.105.141.64/26 -o eth0 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
 
 iptables -A INPUT -j DROP
 iptables -A FORWARD -j DROP
